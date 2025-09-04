4b58f1ff51d3ce7484ff6eb98002564c019342ec
PaceCN <52063709+wx5484@users.noreply.github.com>
2025-09-05 04:41:12 +0800

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).send('Hello World, the proxy is working!');
+  res.status(200).send("Hello World, the proxy is working!");
 }
-```4.  点击 **Commit new file** 保存。
